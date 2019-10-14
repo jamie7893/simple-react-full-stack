140a7f8c22bb51d036625b7a58f079918c66c581
Jamie Keelen <jamie7893@live.com>
2019-10-14 08:53:49 -0500

    customize css and first commit

--- src/client/App.js
+++ src/client/App.js
@@ -1,22 +1,45 @@
 import React, { Component } from 'react';
 import './app.css';
-import ReactImage from './react.png';
+import { Calendar, Views, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import events from './events';
+import * as dates from './dates';
+
+let allViews = Object.keys(Views).map(k => Views[k]);
+
+const localizer = momentLocalizer(moment);
+const myEventsList = [];
+const ColoredDateCellWrapper = ({ children }) =>
+  React.cloneElement(React.Children.only(children), {
+    style: {
+      backgroundColor: 'grey'
+    }
+  });
+const MyCalendar = props => (
+  <div>
+    <Calendar
+      events={events}
+      views={allViews}
+      step={60}
+      showMultiDayTimes
+      max={dates.add(dates.endOf(new Date(2015, 17, 1), 'day'), -1, 'hours')}
+      defaultDate={new Date()}
+      components={{
+        timeSlotWrapper: ColoredDateCellWrapper
+      }}
+      localizer={localizer}
+    />
+  </div>
+);
 
 export default class App extends Component {
-  state = { username: null };
-
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
-  }
+  state = {};
 
   render() {
-    const { username } = this.state;
     return (
-      <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
+      <div id="calendar">
+        <MyCalendar />
       </div>
     );
   }
---
+++ src/client/events.js
@@ -0,0 +1,11 @@
+const now = new Date();
+
+export default [
+  {
+    id: 0,
+    title: 'Fuse Visiting',
+    allDay: true,
+    start: new Date(2019, 9, 1),
+    end: new Date(2019, 9, 31)
+  }
+];
